dc7b8bd900feb6e46dd58b29b812be1549c77be9
luengor <aluengor0001@gmail.com>
2025-01-31 13:53:25 +0100

    cleaner debug page

--- app/mediadebug/page.tsx
+++ app/mediadebug/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useRef, useEffect } from "react";
-import { createPoseLandmarker } from "../media/mediapipe";
+import { createPoseLandmarker, predict } from "../media/mediapipe";
 
 export default function Page() {
     // Get a websocket
@@ -24,59 +24,18 @@ export default function Page() {
     // Create pose landmarker and start detecting
     useEffect(() => {
         if (videoStream) {
+            alert("Ready to send data");
             const isOnMobile = navigator.userAgent.toLowerCase().includes("mobile");
             createPoseLandmarker(isOnMobile ? "lite" : "full").then((poseLandmarker) => {;
-                let lastTime = 0;
-
-                const predict = async () => {
-                    const start = performance.now();
-                    if (lastTime !== inputVideoRef.current.currentTime) {
-                        lastTime = inputVideoRef.current.currentTime;
-                        poseLandmarker.detectForVideo(
-                            inputVideoRef.current,
-                            start,
-                            (result) => {
-                                if (!result.worldLandmarks || !result.worldLandmarks.length) return;
-
-                                // Convert to a json
-                                const worldLandmarks = result.worldLandmarks[0].map((landmark) => {
-                                    return {
-                                        x: landmark.x.toFixed(3),
-                                        y: landmark.y.toFixed(3),
-                                        z: landmark.z.toFixed(3),
-                                    }
-                                });
-                                const imageLandmarks = result.landmarks[0].map((landmark) => {
-                                    return {
-                                        x: landmark.x.toFixed(3),
-                                        y: landmark.y.toFixed(3),
-                                        z: landmark.z.toFixed(3),
-                                    }
-                                });
-
-                                const json = {
-                                    world: worldLandmarks,
-                                    image: imageLandmarks
-                                };
-
-                                const jsonStr = JSON.stringify(json);
-
-                                ws.send(jsonStr);
-                            }
-                        )
-                    }
-
-                    if (inputVideoRef.current.srcObject) {
-                        requestAnimationFrame(predict);
-                    }
-                }
-
-                predict();
+                predict(poseLandmarker, inputVideoRef, (result) => {
+                    if (ws.readyState === ws.OPEN)
+                        ws.send(result);
+                });
 
                 return () => {
                     poseLandmarker.close();
-                    // if (ws)
-                    //     ws.close();
+                    if (ws)
+                        ws.close();
                 }
             });
         }
@@ -91,6 +50,7 @@ export default function Page() {
         <>
         <video
             id="input"
+            className=""
             ref={(r) => {
                 inputVideoRef.current = r;
                 if (!!inputVideoRef.current)
