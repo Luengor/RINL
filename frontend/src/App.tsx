0b20250733f8eb4d01b81b73ca43ed91d7a3da9b
luengor <aluengor0001@gmail.com>
2025-02-24 23:59:04 +0100

    media page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom"
+
 import Login from "./pages/login"
+import Media from "./pages/media" 
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/media" element={<Media />} />
     </Routes>
   )
 }
