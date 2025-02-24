5a1d1c55f6df39c704bac1784a1e82962be1caa9
luengor <aluengor0001@gmail.com>
2025-02-25 00:01:17 +0100

    mediadebug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom"
 
 import Login from "./pages/login"
 import Media from "./pages/media" 
+import MediaDebug from "./pages/mediadebug"
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/media" element={<Media />} />
+      <Route path="/mediadebug" element={<MediaDebug />} />
     </Routes>
   )
 }
