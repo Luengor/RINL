b2df8114027d6ad9c709db5125124d2ea976bfe7
luengor <aluengor0001@gmail.com>
2025-03-01 14:06:25 +0100

    activity tests

--- backend/backend/tests/test_activities.py
+++ backend/backend/tests/test_activities.py
@@ -2,6 +2,29 @@ from .utils import client, login_token
 from .data import test_user
 from datetime import datetime 
 from schemas.users import UserBase
+import pytest
+
+@pytest.fixture
+def create_activity(client, login_token):
+    # Create an activity
+    d = datetime.today().isoformat('T')
+    response = client.post(
+        "/activity/",
+        headers={
+            "Authorization": f"Bearer {login_token}",
+            "Content-Type": "application/json"
+        },
+        json={
+            "date": d, 
+            "minigame": "test",
+            "duration": 100,
+            "activity_points": 100,
+            "extra_data": "{}"
+        }
+    )
+
+    return response.json()
+
 
 def test_create_activity(client, login_token):
     # Create an activity
@@ -22,14 +45,10 @@ def test_create_activity(client, login_token):
     )
 
     assert response.status_code == 200
-    rjson = response.json()
 
-    assert "uuid" in rjson.keys()
-    rjson.pop("uuid")
-
-    print(d)
-    assert rjson == {
+    assert response.json() == {
         "date": d, 
+        "uuid": 1,
         "minigame": "test",
         "duration": 100,
         "activity_points": 100,
@@ -37,4 +56,92 @@ def test_create_activity(client, login_token):
         "user": UserBase(**test_user.model_dump()).model_dump()
     }
         
+def test_get_activity(client, login_token, create_activity):
+    # Get an activity
+    response = client.get(
+        f"/activity/",
+        headers={
+            "Authorization": f"Bearer {login_token}",
+            "Content-Type": "application/json"
+        }
+    )
+
+    assert response.status_code == 200
+    print(response.json())
+    create_activity.pop("user")
+    assert response.json() == [create_activity] 
+
+def test_get_filter_minigate(client, login_token, create_activity):
+    # Get an activity
+    response = client.get(
+        f"/activity/?minigame_filter=test",
+        headers={
+            "Authorization": f"Bearer {login_token}",
+            "Content-Type": "application/json"
+        }
+    )
+
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+
+    response = client.get(
+        f"/activity/?minigame_filter=wrong",
+        headers={
+            "Authorization": f"Bearer {login_token}",
+            "Content-Type": "application/json"
+        }
+    )
+
+    assert response.status_code == 200
+    assert len(response.json()) == 0
+
+def test_get_filter_date(client, login_token, create_activity):
+    # Get an activity
+    response = client.get(
+        f"/activity/?from_date=2000-01-01",
+        headers={
+            "Authorization": f"Bearer {login_token}",
+            "Content-Type": "application/json"
+        }
+    )
+
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+
+    response = client.get(
+        f"/activity/?to_date=2000-01-01",
+        headers={
+            "Authorization": f"Bearer {login_token}",
+            "Content-Type": "application/json"
+        }
+    )
+
+    assert response.status_code == 200
+    assert len(response.json()) == 0
+
+def test_delete_activity(client, login_token, create_activity):
+    # Delete an activity
+    response = client.delete(
+        f"/activity/{create_activity['uuid']}",
+        headers={
+            "Authorization": f"Bearer {login_token}",
+            "Content-Type": "application/json"
+        }
+    )
+
+    assert response.status_code == 200
+    create_activity.pop("user")
+    create_activity.pop("uuid")
+    assert response.json() == create_activity
+
+    response = client.get(
+        f"/activity/",
+        headers={
+            "Authorization": f"Bearer {login_token}",
+            "Content-Type": "application/json"
+        }
+    )
+
+    assert response.status_code == 200
+    assert len(response.json()) == 0
 
