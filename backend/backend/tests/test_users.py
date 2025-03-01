722d5de8d1e00c2ba6c3c0ed43a4b5da1cc11d84
luengor <aluengor0001@gmail.com>
2025-03-01 13:17:30 +0100

    get me test

--- backend/backend/tests/test_users.py
+++ backend/backend/tests/test_users.py
@@ -1,4 +1,4 @@
-from .utils import client, session
+from .utils import client, session, login_token
 from .data import test_user
 from models.user import User
 
@@ -25,4 +25,22 @@ def test_create_user(client):
 
     # Check user in database
     assert session.query(User).filter(User.email == test_user.email).first()
+
+def test_get_me(client, login_token):
+    # Get user
+    response = client.get(
+        "/user/me",
+        headers = {
+            "Authorization": f"Bearer {login_token}"
+        }
+    )
+
+    # Check response
+    assert response.status_code == 200
+    assert response.json() == {
+        "email": test_user.email,
+        "name": test_user.name,
+        "year_of_birth": test_user.year_of_birth,
+        "verified": True 
+    }
 
--- backend/backend/tests/utils.py
+++ backend/backend/tests/utils.py
@@ -6,16 +6,26 @@ import pytest
 from backend.main import app
 from core.db import Base, get_db
 from core.mail import get_send_email
+from core.auth_utils import get_password_hash, create_access_token
+from models.user import User
+from .data import test_user
 
+# Database setup
 engine = create_engine("sqlite:///./test.db")
-Base.metadata.drop_all(engine)
-Base.metadata.create_all(engine)
 session = Session(engine)
 
 @pytest.fixture
 def client():
+    global session, engine
+
     def db():
         return session
+
+    # I don't like this
+    session.close()
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
 
     # Database setup
     app.dependency_overrides[get_db] = db 
@@ -25,3 +35,19 @@ def client():
 
     # Create client
     return TestClient(app)
+
+@pytest.fixture
+def login_token(client):
+    # Add user to database
+    test_user_dict = test_user.model_dump()
+    test_user_dict["hashed_password"] = get_password_hash(test_user.password) 
+    test_user_dict.pop("password")
+    test_user_dict["verified"] = True
+
+    user = User(**test_user_dict)
+    session.add(user)
+    session.commit()
+
+    # Create login token
+    return create_access_token({"sub": user.email})
+
