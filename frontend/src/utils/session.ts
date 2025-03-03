08d1134f2f484833c7f5e7385a8ce7c889852852
luengor <aluengor0001@gmail.com>
2025-03-03 14:10:26 +0100

    session

--- frontend/src/utils/session.ts
+++ frontend/src/utils/session.ts
@@ -1,4 +1,4 @@
-import { loginForTokenLoginPost } from "../client"; 
+import { loginForTokenLoginPost, refreshTokenLoginRefreshPost, createUserUserPost } from "../client"; 
 
 export async function login(email: string, password: string) {
   const response = await loginForTokenLoginPost({
@@ -9,13 +9,43 @@ export async function login(email: string, password: string) {
     }
   })
 
-  localStorage.setItem('access_token', response.data.access_token);
+  if (response.error)
+    throw response.error;
 
-  console.log(response);
-  return response.data;
+  localStorage.setItem('access_token', response.data.access_token);
 }
 
-export function is_logged_in() {
+export function logout() {
+  // Remove the access token
+  localStorage.removeItem('access_token');
+}
+
+export async function register(email: string, password: string, name: string, birthYear: number) {
+  // Register a new user
+  const response = await createUserUserPost({
+    body: {
+      email: email,
+      password: password,
+      name: name,
+      year_of_birth: birthYear
+    }
+  });
+
+  if (response.error)
+    throw response.error;
+
+  return await login(email, password);
+}
+
+export function has_token() {
   // Check if we have an access token
   return localStorage.getItem('access_token') !== null;
 }
+
+export async function refresh_token() {
+  // Refresh the access token
+  const response = await refreshTokenLoginRefreshPost();
+  localStorage.setItem('access_token', response.data.access_token);
+  return response.data;
+}
+
