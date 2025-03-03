94fa617ad2c2ae9263881f11312d443bab068bf4
luengor <aluengor0001@gmail.com>
2025-03-03 01:24:30 +0100

    better login

--- frontend/src/utils/session.ts
+++ frontend/src/utils/session.ts
@@ -1,6 +1,8 @@
 // const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
 const API_URL = 'http://localhost:8000';
 
+import { loginForTokenLoginPost } from "../client"; 
+
 export async function login(email: string, password: string) {
   const request_options = {
     method: 'POST',
@@ -13,14 +15,16 @@ export async function login(email: string, password: string) {
       password: password
     })
   }
+  const response = await loginForTokenLoginPost({
+    body: {
+      grant_type: 'password',
+      username: email,
+      password: password
+    }
+  })
 
-  const response = await fetch(`${API_URL}/login`, request_options);
-  const data = await response.json();
-
-  if (!response.ok) {
-    return null;
-  }
+  localStorage.setItem('access_token', response.data.access_token);
 
   console.log(response);
-  return data;
+  return response.data;
 }
