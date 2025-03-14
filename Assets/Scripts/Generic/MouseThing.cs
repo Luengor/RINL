865c8788beae1470658a0eb3cf8c875b5ec985d3
luengor <aluengor0001@gmail.com>
2025-03-14 13:45:21 +0100

    can press buttons putting the hand over them!!!

---
+++ Assets/Scripts/Generic/MouseThing.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+
+public class MouseThing : MonoBehaviour
+{
+    public Transform target;
+    public float stillClickTime = 2f;
+    public float stillClickDistance = 0.1f;
+
+    private Camera mainCamera;
+    private Vector2 lastPos = Vector2.zero;
+    private float stillTime;
+
+    void OnDrawGizmos()
+    {
+        if (target == null)
+            return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(target.position, stillClickDistance);
+    }
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        // Get on screen position of the target object 
+        Vector2 screenPos = mainCamera.WorldToScreenPoint(target.position);
+
+        // Queue a mouse event at the position of this object
+        InputSystem.QueueStateEvent(Mouse.current, new MouseState {
+            position = screenPos
+        });
+
+        // Check if the mouse is still
+        Vector2 normalizedScreenPos = screenPos / Screen.height;
+        if (Vector2.SqrMagnitude(lastPos - normalizedScreenPos) < stillClickDistance * stillClickDistance)
+        {
+            stillTime += Time.deltaTime;
+
+            if (stillTime >= stillClickTime)
+            {
+                InputSystem.QueueStateEvent(Mouse.current, new MouseState {
+                    position = screenPos,
+                    buttons = 1 << (int)MouseButton.Left
+                });
+                Debug.Log("Click!");
+
+                stillTime = 0;
+            }
+        }
+        else
+        {
+            stillTime = 0;
+            lastPos = normalizedScreenPos;
+        }
+
+        // Send the queued events
+        InputSystem.Update();
+    }
+}
