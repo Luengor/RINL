1bea91a4a769138be7dee1350f0f3455dc987149
luengor <aluengor0001@gmail.com>
2025-06-04 21:36:54 +0200

    both hands can use buttons now

--- unity/Assets/Scripts/Generic/MouseThing.cs
+++ unity/Assets/Scripts/Generic/MouseThing.cs
@@ -1,10 +1,15 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.UI;
 
 public class MouseThing : MonoBehaviour
 {
     public Transform target;
+    public Color circleColor = Color.white;
     public RectTransform circle;
     public float stillClickTime = 2f;
     public float stillClickDistance = 0.1f;
@@ -14,19 +19,17 @@ public class MouseThing : MonoBehaviour
     private Camera mainCamera;
     private Vector2 lastPos = Vector2.zero;
     private float stillTime;
+    private Mouse mouse;
 
-    void OnDrawGizmos()
+    void Awake()
     {
-        if (target == null)
-            return;
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(target.position, stillClickDistance);
+        mouse = InputSystem.AddDevice<Mouse>();
     }
 
     void Start()
     {
         mainCamera = Camera.main;
+        circle.GetComponent<Image>().color = circleColor;
     }
 
     void Update()
@@ -34,6 +37,39 @@ public class MouseThing : MonoBehaviour
         // Get on screen position of the target object 
         Vector2 screenPos = mainCamera.WorldToScreenPoint(target.position);
 
+        // Check if there is a button in the position
+        var eventData = new PointerEventData(EventSystem.current)
+        {
+            position = screenPos
+        };
+        var results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+        bool hasButton = false;
+        for (int i = 0; i < results.Count; i++)
+            if (results[i].gameObject.GetComponent<Button>() != null)
+            {
+                hasButton = true;
+                break;
+            }
+        
+        // If there is no button, exit early
+        if (!hasButton)
+        {
+            if (stillTime > 0)
+            {
+                // Queue a mouse event to move the mouse out
+                InputSystem.QueueStateEvent(mouse, new MouseState
+                {
+                    position = screenPos
+                });
+                stillTime = 0;
+                circle.sizeDelta = Vector2.zero; // Hide the circle
+            }
+
+            return;
+        }
+
+        // If there is button
         // Move the circle to the target position
         circle.position = screenPos;
 
@@ -42,10 +78,11 @@ public class MouseThing : MonoBehaviour
         circle.sizeDelta = new Vector2(circleSize, circleSize);
 
         // Queue a mouse event at the position of this object
-        InputSystem.QueueStateEvent(Mouse.current, new MouseState
+        InputSystem.QueueStateEvent(mouse, new MouseState
         {
             position = screenPos
         });
+        
 
         // Check if the mouse is still
         Vector2 normalizedScreenPos = screenPos / Screen.height;
@@ -55,11 +92,12 @@ public class MouseThing : MonoBehaviour
 
             if (stillTime >= stillClickTime)
             {
-                InputSystem.QueueStateEvent(Mouse.current, new MouseState
+                InputSystem.QueueStateEvent(mouse, new MouseState
                 {
                     position = screenPos,
                     buttons = 1 << (int)MouseButton.Left
                 });
+
                 Debug.Log("Click!");
 
                 stillTime = 0;
