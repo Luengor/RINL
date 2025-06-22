fa8e8da6f40bff6952a23bc1e50c2d37b309c8d7
luengor <aluengor0001@gmail.com>
2025-06-22 20:13:27 +0200

    conexiones

--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[FRONTEND_URL],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
