be6ed9453c69311a4de1cfd13e2eece34314468c
luengor <aluengor0001@gmail.com>
2025-07-14 23:57:14 +0200

    change this

--- backend/src/backend/create_test_data.py
+++ backend/src/backend/create_test_data.py
@@ -15,20 +15,20 @@ POSTGRES_USER = environ.get("POSTGRES_USER", "postgres")
 POSTGRES_PASSWORD = environ.get("POSTGRES_PASSWORD", "postgres")
 POSTGRES_HOST = environ.get("POSTGRES_HOST", "localhost")
 
-puser = input(f"Postgres user ({POSTGRES_USER}): ") or POSTGRES_USER
-ppass = input(
-    f"Postgres password ({POSTGRES_PASSWORD}): ") or POSTGRES_PASSWORD
-phost = input(f"Postgres host ({POSTGRES_HOST}): ") or POSTGRES_HOST
-ENGINE_PATH = f"postgresql://{puser}:{ppass}@{phost}:5432"
-engine = create_engine(ENGINE_PATH)
-
 ACTIVITY_COUNT = 2000
 ACTIVITIES_FROM = 100
-MINIGAMES = ["balloons", "dodge"]
+MINIGAMES = ["Globos", "Esquivar"]
 
 
 def main():
     """Main function that creates the test data in the database."""
+
+    puser = input(f"Postgres user ({POSTGRES_USER}): ") or POSTGRES_USER
+    ppass = input(
+        f"Postgres password ({POSTGRES_PASSWORD}): ") or POSTGRES_PASSWORD
+    phost = input(f"Postgres host ({POSTGRES_HOST}): ") or POSTGRES_HOST
+    engine_path = f"postgresql://{puser}:{ppass}@{phost}:5432"
+    engine = create_engine(engine_path)
 
     Base.metadata.create_all(engine)
     now = datetime.now(timezone.utc)
